4572f41611268f0fdbdfde6f40e114f25268b158
sypper-pit <uuhn.info@gmail.com>
2024-06-10 00:45:06 +0300

    Update set_configuration.sh

--- set_configuration.sh
+++ set_configuration.sh
@@ -19,11 +19,3 @@ docker exec -u www-data app-server php occ --no-warnings config:system:set onlyo
 docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice DocumentServerInternalUrl --value="http://onlyoffice-document-server/"
 docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice StorageUrl --value="http://nginx-server/"
 docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice jwt_secret --value="secret"
-
-# Добавляем конфигурацию Redis, если она еще не добавлена
-REDIS_CONFIG="\n  'memcache.local' => '\\OC\\Memcache\\Redis',\n  'redis' => array(\n    'host' => 'redis',\n    'port' => 6379,\n  ),"
-
-# Проверяем, есть ли конфигурация Redis
-if ! docker exec -u www-data app-server grep -q "'redis'" /var/www/html/config/config.php; then
-    docker exec -u www-data app-server bash -c "sed -i \"s|);|$REDIS_CONFIG\n);|\" /var/www/html/config/config.php"
-fi
